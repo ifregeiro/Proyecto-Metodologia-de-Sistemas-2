81339aba5f832c292d6cb4619b287ceca23679fe
Gabriel <gkessleralvarez@gmail.com>
2025-11-08 18:32:41 -0300

    correcciones

--- backend/routes/productos.routes.js
+++ backend/routes/productos.routes.js
@@ -1,12 +1,17 @@
 import { Router } from 'express'
 import {readJSON} from './utils.js'
+import { FarmaciaModel } from '../models/farmaciaDB.js'
 const router = Router()
 
-router.get('/', (req, res) =>{
+router.get('/producto', (req, res) =>{
     const { categoria } = req.query
     if (categoria) {
         const filtradoProductos = productos.filter( producto => producto.categoria.some(c => c.toLowerCase() === categoria.toLowerCase()))
         return res.json(filtradoProductos)
     }
     res.json(productos)
+})
+
+router.post('/', (req,res) => {
+    const nuevoProducto = req.body
 })
--- backend/schemas/farmaciaDB.js
+++ backend/schemas/farmaciaDB.js
@@ -1,7 +1,8 @@
 import z from 'zod'
-const ProductoSchema = z.object({
+
+export const ProductoSchema = z.object({
     nombre : z.string({
-        invalid_type_error: 'El nombre del producto debe serun string',
+        invalid_type_error: 'El nombre del producto debe ser un string',
         requiered_error: 'El producto requiere un nombre.'
     }),
     descripcion: z.string(),
