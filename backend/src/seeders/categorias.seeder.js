73ed4bf83e501eed8c2174f1bfe3bbe41460e96b
Gabriel0258 <gkessleralvarez@gmail.com>
2025-11-28 22:44:49 -0300

    Seeders Creados

--- backend/src/seeders/categorias.seeder.js
+++ backend/src/seeders/categorias.seeder.js
@@ -2,9 +2,11 @@ import { Categoria } from "../models/Categoria.model"
 
 export async function categoriaSeed() {
     await Categoria.bulkCreate([
-        {nombre: "Medicamento"},
-        {nombre: "Cuidado Personal"},
-        {nombre: "Perfumería"}
+        {nombre: 'Medicamento'},
+        {nombre: 'Cuidado Personal'},
+        {nombre: 'Vitaminas y Suplementos'},
+        {nombre: 'Higiene'},
+        {nombre: 'Bebés y Maternidad'}
     ])
     console.log("Categorías Subidas")
 }
